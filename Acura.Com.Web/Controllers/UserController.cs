07a56159ef699de88913783a0dcc27aa21b31582
johnatan-garcia <johnatan.garcia@gmail.com>
2014-11-13 10:31:30 -0500

    Technologies

--- Acura.Com.Web/Controllers/UserController.cs
+++ Acura.Com.Web/Controllers/UserController.cs
@@ -14,6 +14,7 @@ namespace Acura.Com.Web.Controllers
             var technologyList = new List<Technology>();
             technologyList.Add(new Technology { Icon = "desktop", Name = "Responsive", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse interdum erat et neque tincidunt volutpat. Cras eget augue id dui varius pretium." });
             technologyList.Add(new Technology { Icon = "css3", Name = "CSS3", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse interdum erat et neque tincidunt volutpat. Cras eget augue id dui varius pretium." });
+            technologyList.Add(new Technology { Icon = "lightbulb-o", Name = "Javascript", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse interdum erat et neque tincidunt volutpat. Cras eget augue id dui varius pretium." });
 
             return PartialView(technologyList);
         }
